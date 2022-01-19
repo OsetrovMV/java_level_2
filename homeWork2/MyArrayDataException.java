376ad01b42e5794cbc600c62b7d9a58373b9536f
OsetrovMV <85795324+OsetrovMV@users.noreply.github.com>
2022-01-19 20:53:07 +0300

    Загрузил ДЗ №2 по java2

---
+++ homeWork2/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.javacore.qa.homeWork2;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
---
+++ homeWork2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.javacore.qa.homeWork2;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
