a534b67a96effed8730ed2c213c71d1b57fae5ea
OsetrovMV <85795324+OsetrovMV@users.noreply.github.com>
2022-01-24 21:27:28 +0300

    Удалены файлы 2-го ДЗ из папки 3-го ДЗ и добавлено ДЗ №3

--- homeWork2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.javacore.qa.homeWork2;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
-
--- homeWork2/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.javacore.qa.homeWork2;
-
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
